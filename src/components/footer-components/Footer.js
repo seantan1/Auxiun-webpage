c8e8d6f398e59e38c2aee226b975af3b099ead8e
Clinton Pham <Clintonjob@hotmial.co.uk>
2021-07-27 20:37:37 +1000

    Progress on footer. Need to align subscribe btn

--- src/components/footer-components/Footer.js
+++ src/components/footer-components/Footer.js
@@ -1,16 +1,131 @@
-import './css/Footer.css';
-const Footer = () => {
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-
-    return (
-        <div className="footer">
-            <div className="footer-links-container">
-                Footer
-            </div>
-            
-        </div>
-
-    );
+import "./css/Footer.css";
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Capstone NFT
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-export default Footer;
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+const footers = [
+  {
+    title: "PRODUCTS",
+    description: ["Platform", "Wallet", "Marketplace"],
+  },
+  {
+    title: "SOLUTIONS",
+    description: ["Gaming", "Cryptocurrency", "Software", "Developer"],
+  },
+  {
+    title: "Resources",
+    description: ["Blog", "Documentation", "Roadmap", "Timeline"],
+  },
+  {
+    title: "About",
+    description: ["Why Us?", "Company", "Team", "Careers", "Contact Us"],
+  },
+];
+
+export default function Pricing() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <Container maxWidth="md" component="footer" className={classes.footer}>
+        <Grid container spacing={4} justifyContent="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link href="#" variant="subtitle1" color="textSecondary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid>
+        <hr />
+{/* NEED TO ALIGN SUBSCRIBE BTN PROPERLY */}
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={5}>
+            <Typography variant="h6">Stay Up To Date</Typography>
+            <Typography variant="caption">
+              Don't miss the latest news, guides and insights.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="outlined-basic"
+              label="Email Address"
+              variant="outlined"
+              helperText="We will never share your email with anyone else"
+            />
+            <Button className="subscribe-btn" variant="contained" color="primary">
+              Subscribe
+            </Button>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
+}
