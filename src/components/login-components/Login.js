f3aabfb4d1e58b20d2fe4c4f6d30eda97666055d
seantan1 <seantan374@gmail.com>
2021-08-01 23:55:40 +1000

    implemented login function with axios

--- src/components/login-components/Login.js
+++ src/components/login-components/Login.js
@@ -113,131 +113,130 @@ import Container from "@material-ui/core/Container";
 
 const axios = require("axios");
 
- 
+
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    // marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    marginTop: theme.spacing(13),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(0),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    paper: {
+        // marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        marginTop: theme.spacing(13),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(0),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 const Login = (props) => {
-  // login form
-  const [userEmailLoginForm, setuserEmailLoginForm] = useState("");
-  const [userPasswordLoginForm, setUserPasswordLoginForm] = useState("");
+    // login form
+    const [userEmailLoginForm, setuserEmailLoginForm] = useState("");
+    const [userPasswordLoginForm, setUserPasswordLoginForm] = useState("");
 
-  // event handler for username
-  const userEmailLoginFormHandler = (event) => {
-    setuserEmailLoginForm(event.target.value);
-  };
-  // event handler for password
-  const userPasswordLoginFormHandler = (event) => {
-    setUserPasswordLoginForm(event.target.value);
-  };
+    // event handler for username
+    const userEmailLoginFormHandler = (event) => {
+        setuserEmailLoginForm(event.target.value);
+    };
+    // event handler for password
+    const userPasswordLoginFormHandler = (event) => {
+        setUserPasswordLoginForm(event.target.value);
+    };
 
-  const login = () => {
-    axios
-      .post(process.env.REACT_APP_API_USER_LOGIN_URL, {
-        apikey: process.env.REACT_APP_PRIVATE_API_KEY,
-        email: String(userEmailLoginForm),
-        password_hash: String(userPasswordLoginForm),
-      })
-      .then(function (data) {
-        if (data.data.message === "fail") {
-          console.log("Username or password is incorrect");
-          // TODO: set alert using props from App.js
-        } else if (data.data.message === "success") {
-          console.log("Login successful!");
-          // TODO: set alert using props from App.js, set authenticated useStates from App.js
-          // setUserId(data.data.data[0]._id);
-          // setUserUsername(data.data.data[0].username);
-          // setAuthenticated(true);
-        } else {
-          console.log("Unknown error occured. Please try again.");
-          // TODO: set alert using props from App.js
-        }
-      });
-  };
-  const classes = useStyles();
+    const login = () => {
+        axios.post(process.env.REACT_APP_DATABASE_API_LOGIN_URL, {
+            apikey: process.env.REACT_APP_DATABASE_API_KEY,
+            email: String(userEmailLoginForm),
+            password_hash: String(userPasswordLoginForm),
+        })
+            .then(function (data) {
+                if (data.data.message === "fail") {
+                    console.log("Email or password is incorrect");
+                    // TODO: set alert using props from App.js
+                } else if (data.data.message === "success") {
+                    console.log("Login successful!");
+                    // TODO: set alert using props from App.js, set authenticated useStates from App.js
+                    // setUserId(data.data.data[0]._id);
+                    // setUserUsername(data.data.data[0].username);
+                    // setAuthenticated(true);
+                } else {
+                    console.log("Unknown error occured. Please try again.");
+                    // TODO: set alert using props from App.js
+                }
+            });
+    };
+    const classes = useStyles();
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="Username"
-            label="Username"
-            autoComplete="email"
-            autoFocus
-            onChange={userEmailLoginFormHandler}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            onChange={userPasswordLoginFormHandler}
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/register" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      
-    </Container>
-  );
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="Email"
+                        label="Email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={userEmailLoginFormHandler}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        onChange={userPasswordLoginFormHandler}
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={login}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/register" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+
+        </Container>
+    );
 };
 
 export default Login;
