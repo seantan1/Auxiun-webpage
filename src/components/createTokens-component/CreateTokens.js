1074b7cd0428a37f54969427a2c3f1a0e6281295
Clinton Pham <Clintonjob@hotmial.co.uk>
2021-08-10 18:55:42 +1000

    Progress on mint page

--- src/components/createTokens-component/CreateTokens.js
+++ src/components/createTokens-component/CreateTokens.js
@@ -1,95 +1,123 @@
 import React from "react";
 import "./css/CreateTokens.css";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
-import { withStyles } from "@material-ui/core/styles";
-import Gravatar from "react-gravatar";
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-    borderRadius: "5em",
-    textDecoration: "none",
-  },
-  input: {
-    display: "none",
-  },
-
-  chip: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
   },
 }));
-const StyledButton = withStyles({
-  root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    borderRadius: "5em",
-    border: 0,
-    color: "white",
-    height: 28,
-    padding: "0 10px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  },
-  label: {
-    textTransform: "capitalize",
-    fontSize: 10,
-  },
-})(Button);
 
-const CreateTokens = () => {
+// sample data
+const gamePlatforms = [
+  {
+    value: "1",
+    label: "Fortnite",
+  },
+  {
+    value: "2",
+    label: "CSGO",
+  },
+];
+
+const sampleProductData = [
+  {
+    value: "1",
+    label: "Knife",
+  },
+  {
+    value: "2",
+    label: "Pickaxe",
+  },
+];
+
+export default function CreateTokens() {
   const classes = useStyles();
+  const [gamePlatform, setGamePlatform] = React.useState("");
+  const handleChange = (event) => {
+    setGamePlatform(event.target.value);
+  };
 
   return (
     <div>
-      <div className="profile-banner profile-text">
-        <div className="profile-banner-background"></div>
+      <div className="token-banner">
+        <div className="token-banner-background"></div>
 
-        {/* user email goes in email */}
-        <Gravatar email="" size={100} rounded />
-        <button className="edit-btn">Edit Profile</button>
+        <div className="form-align">
+          <h4></h4>
+          <form className={classes.root} noValidate autoComplete="off">
+            <div>
+              <TextField
+                required
+                id="game-id"
+                label="Game Platform"
+                select
+                onChange={handleChange}
+                helperText="Please select which gaming platform"
+              >
+                {gamePlatforms.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                id="standard-select-currency"
+                select
+                label="Product"
+                //   value={currency}
+                onChange={handleChange}
+                helperText="Please select which item to add"
+              >
+                {sampleProductData.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                id="standard-number"
+                label="Amount"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id="ether-price"
+                label="Ether Price"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
 
-        <div className="user-title">[Username]</div>
-
-        <div className="wallet-title">[Wallet Address]</div>
-
-        <div className="email-title">[EMAIL]</div>
-
-        <div className="link-btn">
-          Link the following platforms to your profile
-          <br />
-          <button className="game-icon">
-            {" "}
-            <i className="steam icon"></i>
-          </button>
-          <button className="game-icon">
-            {" "}
-            <i className="steam icon"></i>
-          </button>
+              <div className={classes.root}>
+                  <span className="upload-title">Upload Image </span>
+                <label htmlFor="contained-button-file"></label>
+                <input
+                  variant="contained"
+                  color="primary"
+                  component="span"
+                  accept="image/*"
+                  className={classes.input}
+                  id="icon-button-file"
+                  type="file"
+                />
+              </div>
+            </div>
+            <button className="create-btn">Create NFT</button>
+          </form>
         </div>
       </div>
-
-      <Container className="bottom-list">
-        <Typography variant="overline" display="block" gutterBottom>
-          <p className="">
-            You have reached the bottom of the list
-            <br />
-            Nothing here yet! Check our <strong>new drops</strong> or explore
-            <br /> more options <strong>marketplace page</strong>
-          </p>
-        </Typography>
-        <div className="banner-text">
-          <StyledButton>
-            <a className="banner-text" href="/marketplace">
-              Explore Marketplace
-            </a>
-          </StyledButton>
-        </div>
-      </Container>
     </div>
   );
-};
-
-export default CreateTokens;
+}
