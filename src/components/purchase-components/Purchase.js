26a3cdb75ca5eae9ff7b23a9954cabe639268c61
Nikita Phung <nikitaphung2000@gmail.com>
2021-08-10 15:24:21 +1000

    Made axios call to backend api for purchasing NFT

--- src/components/purchase-components/Purchase.js
+++ src/components/purchase-components/Purchase.js
@@ -6,12 +6,31 @@ import {
     TOKEN_CONTRACT_ADDRESS,
     TOKEN_CONTRACT_ABI
 } from "../../contract-data/token-contract-data";
-
+const axios = require('axios');
 const Purchase = (props) => {
+    // Need to change to actual route
+    const purchaseRoute = '/purchase/:tokenId';
 
     // Method for purchasing NFT
-    const purchaseNFT = () => {
-        const web3 = new Web3(window.ethereum)
+    const purchaseNFT = (tokenId) => {
+        const web3 = new Web3(window.ethereum);
+        const contract = new web3.eth.Contract(TOKEN_CONTRACT_ABI, TOKEN_CONTRACT_ADDRESS);
+        contract.methods.purchaseNFT(tokenId).call()
+        .then((result) => {
+            const timestamp = new Date().toISOString()
+            const transaction = {
+                'transaction_id': timestamp + tokenId,
+                'buyer': result[0], 
+                'seller': result[1],
+                'token_id': result[2],
+                'amount': result[3],
+                'timestamp': timestamp
+            }
+            axios.post(purchaseRoute, transaction)
+            .then(response => {
+               // Need to error check
+            })
+        })
     }
 
 
