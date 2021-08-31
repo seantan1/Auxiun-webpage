97c02ed787d61a5a9194506165842869f1523bf4
Clinton Pham <Clintonjob@hotmial.co.uk>
2021-08-31 18:25:51 +1000

    Update Profile form complete

--- src/components/profile-component/UpdateProfile.js
+++ src/components/profile-component/UpdateProfile.js
@@ -1,39 +1,38 @@
 import { useState, useEffect } from "react";
 import "./css/updateProfile.css";
 
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
 const UpdateProfile = (props) => {
   return (
     <form>
       <div class="page">
         <h3>Update Profile </h3>
 
-        <label class="field field_v1">
-          <input
-            class="field__input"
-            placeholder={props.userSessionData.firstname}
-          />
-          <span class="field__label-wrap">
-            <span class="field__label">First name</span>
-          </span>
-        </label>
-        <label class="field field_v2">
-          <input
-            class="field__input"
-            placeholder={props.userSessionData.lastname}
-          />
-          <span class="field__label-wrap">
-            <span class="field__label">Last name</span>
-          </span>
-        </label>
-        <label class="field field_v3">
-          <input
-            class="field__input"
-            placeholder={props.userSessionData.email}
-          />
-          <span class="field__label-wrap">
-            <span class="field__label">E-mail</span>
-          </span>
-        </label>
+        <TextField
+          id="firstname"
+          label="First Name"
+          placeholder={props.userSessionData.firstname}
+          helperText="Leave blank if you do not wish to change"
+        />
+
+        <TextField
+          id="lastname"
+          label="Last Name"
+          placeholder={props.userSessionData.lastname}
+          helperText="Leave blank if you do not wish to change"
+        />
+        
+
+        <TextField
+          label="Email"
+          disabled
+          value={props.userSessionData.email}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
 
         <a>Change Password</a>
         <button>Update Profile</button>
