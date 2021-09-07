708d6ad2608355b3ee7646d513effffd3a75c691
s3742332 <s3742332@student.rmit.edu.au>
2021-09-07 21:19:44 +1000

    dark theme responsive

--- src/components/faq-components/Faq.js
+++ src/components/faq-components/Faq.js
@@ -14,35 +14,40 @@ export default function Pricing() {
     return (
         <div className='faq-content'>
             <h1 className='faq-heading'>General</h1>
-            <Accordion className='faq-accordian'>
+            <Accordion className='faq-accordian' style={{ color: darkTheme ? 'aliceblue' : '' }}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
+                    style={{ backgroundColor: darkTheme ? '#424242' : '' }}
                 >
                     <Typography className='faq-question'>Why Can't I see Anything in the Marketplace?</Typography>
                 </AccordionSummary>
-                <AccordionDetails className='faq-answer'>
-                    <Typography>
+                <AccordionDetails className='faq-answer' style={{ backgroundColor: darkTheme ? '#4C4C4C' : 'lightgray' }}>
+                    <Typography >
                         Users are required to login and connect their Metamask accounts via the Metamask extension in order to see NFTs within the marketplace.
                     </Typography>
                 </AccordionDetails>
-            </Accordion><Accordion className='faq-accordian'>
+            </Accordion>
+            <Accordion className='faq-accordian' style={{ color: darkTheme ? 'aliceblue' : '' }}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
+                    style={{ backgroundColor: darkTheme ? '#424242' : '' }}
                 >
                     <Typography className='faq-question'>Why Can't I Create New Tokens?</Typography>
                 </AccordionSummary>
-                <AccordionDetails className='faq-answer'>
+                <AccordionDetails className='faq-answer' style={{ backgroundColor: darkTheme ? '#4C4C4C' : 'lightgray' }}>
                     <Typography>
                         Currently, only approved administrators can mint new tokens.
                     </Typography>
                 </AccordionDetails>
-            </Accordion><Accordion className='faq-accordian'>
+            </Accordion>
+            <Accordion className='faq-accordian' style={{ color: darkTheme ? 'aliceblue' : '' }}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
+                    style={{ backgroundColor: darkTheme ? '#424242' : '' }}
                 >
                     <Typography className='faq-question'>How do I Unsubscribe from the Newsletter?</Typography>
                 </AccordionSummary>
-                <AccordionDetails className='faq-answer'>
+                <AccordionDetails className='faq-answer' style={{ backgroundColor: darkTheme ? '#4C4C4C' : 'lightgray' }}>
                     <Typography>
                         Users can unsubscribe from the footer of every newsletter email sent.
                     </Typography>
